d8dc767d99145387d0eb5603a40087513a1a8f76
HieuTran <trantrunghieu777888555@gmail.com>
2022-01-17 20:44:23 +0700

    feat(lab03)

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Counter } from '../components/counter/couter';
+
+
+const Index = props => {
+    return (
+        <div>
+            <h1>This is example for redux</h1>
+            <Counter />
+        </div>
+    );
+};
+
+
+export default Index;
